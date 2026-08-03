0fabbfb49425c394f30ec049134358cb249b9421
yuru-c <shannon0137@gmail.com>
2026-08-03 22:28:30 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-x = float(input("what's x? "))
+"""x = float(input("what's x? "))
 y = float(input("what's y? "))
 
 # z = round(x + y)
@@ -9,4 +9,14 @@ y = float(input("what's y? "))
 
 z = x / y
 
-print(f"{z:.2f}")
+print(f"{z:.2f}")"""
+
+def main():
+    x = int(input("what's x? "))
+    print("x squared is", square(x))
+
+def square(n):
+    # return n * n
+    return pow(n, 2)
+
+main()

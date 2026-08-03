e9c3d0c8d3a322a2b306900606ec12d8afd969c5
yuru-c <shannon0137@gmail.com>
2026-08-03 21:18:20 +0800

    Add hello.py

---
+++ hello.py
@@ -0,0 +1,17 @@
+# name = input("what is your name?")
+
+# remove whitspace from str
+# name = name.strip()
+
+# capitalize user's name
+# name = name.capitalize()
+# name = name.title()
+
+# name = name.strip().title()
+
+name = input("what is your name?").strip().title()
+
+# split user's name inton first name and last name
+# first, last = name.split(" ")
+
+print(f"Hello, {name}")

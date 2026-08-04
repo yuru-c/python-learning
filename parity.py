4655397a32c9403ec5ad0c9474b3736a1113e3dd
yuru-c <shannon0137@gmail.com>
2026-08-04 13:20:37 +0800

    Update parity.py

--- parity.py
+++ parity.py
@@ -1,6 +1,25 @@
-x = int(input("What's x? "))
+"""x = int(input("What's x? "))
 
 if x % 2 == 0:
     print("Even")
 else:
-    print("Odd")
+    print("Odd")"""
+
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+def is_even(n):
+    """if n % 2 == 0:
+        return True
+    else:
+        return False"""
+    
+    # return True if n % 2 == 0 else False
+
+    return n % 2 == 0
+
+main()

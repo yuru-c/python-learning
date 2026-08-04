ac955220042f63b7a10adca69eaaaccf80e80914
yuru-c <shannon0137@gmail.com>
2026-08-04 14:33:22 +0800

    Add house.py

---
+++ house.py
@@ -0,0 +1,16 @@
+name = input("What's your name? ")
+
+"""if name == "Harry" or name == "Hermione" or name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")"""
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")

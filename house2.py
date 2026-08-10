58c091a856138647f62f742f35fd3a0956ba9058
yuru-c <shannon0137@gmail.com>
2026-08-10 10:45:06 +0800

    Add house2.py (set)

---
+++ house2.py
@@ -0,0 +1,21 @@
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"}
+]
+
+# list house = list()
+'''houses = []
+for student in students:
+    if student["house"] not in houses:
+        houses.append(student["house"])'''
+
+# set 過濾重複的
+houses = set()
+for student in students:
+    houses.add(student["house"])
+
+for house in sorted(houses):
+    print(house)

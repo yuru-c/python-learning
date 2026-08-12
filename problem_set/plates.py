81e607a07aa2f9832ac72266e562efad2fc96c15
yuru-c <shannon0137@gmail.com>
2026-08-12 22:42:54 +0800

    CS50P Pset 2: Complete Vanity Plates

---
+++ problem_set/plates.py
@@ -0,0 +1,45 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+def is_valid(s):
+    # if 2 <= len(s) <= 6 and s[0:2].isalpha() and s.isalnum():
+    #     for i in range(len(s) - 1):
+    #         if s[i].isnumeric() and s[i+1].isalpha():
+    #             return False
+    #     for p in s:
+    #         if p.isdigit():
+    #             if p == "0":
+    #                 return False 
+    #             break
+    #     return True        
+    # else:
+    #     return False
+
+
+    if not 2 <= len(s) <=6:
+        return False
+
+    if not s[:2].isalpha():
+        return False
+
+    if not s.isalnum():
+        return False
+
+    for i in range(len(s) - 1):
+        if s[i].isdigit() and s[i + 1].isalpha():
+            return False
+
+    for i in range(len(s)):
+        if s[i].isdigit():
+            if s[i] == "0":
+                return False
+            break
+
+    return True
+
+if __name__ == "__main__":
+    main()

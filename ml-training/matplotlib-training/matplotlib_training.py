741784cddf8870131a63aa98971d3bdf17415b19
yuru-c <shannon0137@gmail.com>
2026-08-21 14:44:12 +0800

    ml-training-matplotlib matplotlib-training.py line plot

---
+++ ml-training/matplotlib-training/matplotlib_training.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+# subjects = ["Math", "English", "Science", "History", "Physics"]
+# scores = [85, 78, 92, 70, 88]
+# X 軸 → subjects Y 軸 → scores
+
+# plt.plot(subjects, scores, marker=".", linestyle=":")
+# o s ^
+#"-" → 實線 "--" → 虛線 "-." → 點畫線 ":" → 點線
+# plt.title("Student Scores")
+# plt.xlabel("Subject")
+# plt.ylabel("Score")
+
+
+subjects = ["Math", "English", "Science", "History", "Physics"]
+
+class_a = [85, 78, 92, 70, 88]
+class_b = [75, 85, 80, 90, 82]
+
+# help(plt.legend)
+plt.plot(subjects, class_a, label="Class_a")
+plt.plot(subjects, class_b, label="Class_b")
+plt.legend()
+plt.show()

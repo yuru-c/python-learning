e11c12924e1d4e4cfa7d5d71e4f06a545a53a247
yuru-c <shannon0137@gmail.com>
2026-08-21 14:56:37 +0800

    ml-training-matplotlib matplotlib-training.py bar chart

--- ml-training/matplotlib-training/matplotlib_training.py
+++ ml-training/matplotlib-training/matplotlib_training.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 
+# line plot
+
 # subjects = ["Math", "English", "Science", "History", "Physics"]
 # scores = [85, 78, 92, 70, 88]
 # X 軸 → subjects Y 軸 → scores
-
 # plt.plot(subjects, scores, marker=".", linestyle=":")
 # o s ^
 #"-" → 實線 "--" → 虛線 "-." → 點畫線 ":" → 點線
@@ -12,13 +13,21 @@ import matplotlib.pyplot as plt
 # plt.ylabel("Score")
 
 
+# subjects = ["Math", "English", "Science", "History", "Physics"]
+# class_a = [85, 78, 92, 70, 88]
+# class_b = [75, 85, 80, 90, 82]
+# # help(plt.legend)
+# plt.plot(subjects, class_a, label="Class_a")
+# plt.plot(subjects, class_b, label="Class_b")
+# plt.legend()
+# plt.show()
+
+
+# bar chart
 subjects = ["Math", "English", "Science", "History", "Physics"]
-
-class_a = [85, 78, 92, 70, 88]
-class_b = [75, 85, 80, 90, 82]
-
-# help(plt.legend)
-plt.plot(subjects, class_a, label="Class_a")
-plt.plot(subjects, class_b, label="Class_b")
-plt.legend()
+scores = [85, 78, 92, 70, 88]
+plt.bar(subjects, scores)
+plt.title("Student Scores")
+plt.xlabel("Subject")
+plt.ylabel("Score")
 plt.show()

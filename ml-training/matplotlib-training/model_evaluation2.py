6099d8ddf34060bbc36ed3b00e816a79b05d6141
yuru-c <shannon0137@gmail.com>
2026-08-23 21:56:40 +0800

    ml-training-matplotlib model_evaluation2.py (polynomial degree3)

--- ml-training/matplotlib-training/model_evaluation2.py
+++ ml-training/matplotlib-training/model_evaluation2.py
@@ -7,45 +7,45 @@ from sklearn.metrics import (
     max_error, mean_absolute_error, mean_squared_error, r2_score
 )
 
-'''data = {
-    "hours": [1, 2, 3, 4, 5, 6, 7, 8],
-    "score": [52, 58, 65, 71, 78, 84, 90, 95]
-}
+# data = {
+#     "hours": [1, 2, 3, 4, 5, 6, 7, 8],
+#     "score": [52, 58, 65, 71, 78, 84, 90, 95]
+# }
 
-df = pd.DataFrame(data)
-x = df[["hours"]]
-y = df["score"]
+# df = pd.DataFrame(data)
+# x = df[["hours"]]
+# y = df["score"]
 
-x_train, x_test, y_train, y_test = train_test_split(
-    x,y,
-    test_size=0.2, 
-    random_state=42
-)
+# x_train, x_test, y_train, y_test = train_test_split(
+#     x,y,
+#     test_size=0.2, 
+#     random_state=42
+# )
 
-model = LinearRegression(fit_intercept=True)
-model.fit(x_train, y_train)
-y_pred = model.predict(x_test)
-print(y_test)
-print(y_pred)
+# model = LinearRegression(fit_intercept=True)
+# model.fit(x_train, y_train)
+# y_pred = model.predict(x_test)
+# print(y_test)
+# print(y_pred)
 
-# residual = y_test - y_pred
-# print(residual)
+# # residual = y_test - y_pred
+# # print(residual)
 
-df["residual"] = y - model.predict(x)
-# Residual > 0 → Actual > Predicted → 模型低估
-# Residual < 0 → Actual < Predicted → 模型高估
-print(df)
-# print(x)
-# print(model.coef_)
-# print(model.intercept_)
-# print(model.predict(x))
+# df["residual"] = y - model.predict(x)
+# # Residual > 0 → Actual > Predicted → 模型低估
+# # Residual < 0 → Actual < Predicted → 模型高估
+# print(df)
+# # print(x)
+# # print(model.coef_)
+# # print(model.intercept_)
+# # print(model.predict(x))
 
-plt.scatter(x, df["residual"])
-plt.axhline(0)
-# 畫出 Residual = 0 的水平線
-plt.show()
-# 殘差在 0 附近隨機分布 沒有很明顯的曲線 => Linear Regression 很適合描述這組簡單資料
-# 如果殘差有明顯規律 模型可能還沒有抓到資料中的某些模式'''
+# plt.scatter(x, df["residual"])
+# plt.axhline(0)
+# # 畫出 Residual = 0 的水平線
+# plt.show()
+# # 殘差在 0 附近隨機分布 沒有很明顯的曲線 => Linear Regression 很適合描述這組簡單資料
+# # 如果殘差有明顯規律 模型可能還沒有抓到資料中的某些模式
 
 data2 = {
     "x": [1, 2, 3, 4, 5, 6, 7, 8],
@@ -64,17 +64,17 @@ x2_train, x2_test, y2_train, y2_test = train_test_split(
 
 model = LinearRegression(fit_intercept=True)
 model.fit(x2_train, y2_train)
-# df2["residual2"] = y2 - model.predict(x2)
-# print(df2["residual2"])
-# plt.scatter(x2, df2["residual2"])
-# plt.axhline(0)
-# plt.show()
+# # df2["residual2"] = y2 - model.predict(x2)
+# # print(df2["residual2"])
+# # plt.scatter(x2, df2["residual2"])
+# # plt.axhline(0)
+# # plt.show()
 y2_pred_linear = model.predict(x2)
 r2_linear = r2_score(y2, y2_pred_linear)
-print(r2_linear)
+print("Linear", r2_linear)
 
 
-# Polynomial Regression y=ax²+bx+c
+# # Polynomial Regression y=ax²+bx+c
 poly = PolynomialFeatures(degree=2)
 x_poly = poly.fit_transform(x2)
 # print(x_poly)
@@ -88,12 +88,19 @@ y2_pred_poly = model_poly.predict(x_poly)
 df2["residual_poly"] = y2 - y2_pred_poly
 
 r2_poly = r2_score(y2, y2_pred_poly)
-print(r2_poly)
+print("Polynomial2", r2_poly)
 
-plt.scatter(x2, df2["residual_poly"])
-plt.axhline(0)
-plt.show()
+# plt.scatter(x2, df2["residual_poly"])
+# plt.axhline(0)
+# plt.show()
 
+poly3 = PolynomialFeatures(degree=3)
+x_poly3 = poly3.fit_transform(x2)
+model_poly3 = LinearRegression()
+model_poly3.fit(x_poly3, y2)
+y2_pred_poly3 = model_poly3.predict(x_poly3)
+r2_poly3 = r2_score(y2, y2_pred_poly3)
+print("Polynomial3", r2_poly3)
 
 
 
